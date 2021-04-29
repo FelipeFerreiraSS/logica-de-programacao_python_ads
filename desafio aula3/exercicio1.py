0490190abd3b49c046ef2ed2fb8e535abc0b09ad
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-04-28 23:28:56 -0300

    exercio1

---
+++ desafio aula3/exercicio1.py
@@ -0,0 +1,9 @@
+# Operadores logicos
+m1 = float(input("Digite a 1° materia: "))
+m2 = float(input("Digite a 2° materia: "))
+m3 = float(input("Digite a 3° materia: "))
+
+if m1 >= 7 and m2 >= 7 and m3 >=7:
+    print('Aluno aprovado!')
+else:
+    print('Aluno reprovado!')
