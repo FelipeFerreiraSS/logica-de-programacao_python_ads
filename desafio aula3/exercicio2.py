4908115130c183402526394229957f70fe745b9a
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-04-29 00:02:13 -0300

    Create exercicio2.py

---
+++ desafio aula3/exercicio2.py
@@ -0,0 +1,20 @@
+print('O que deseja comprar?')
+print('1 - maça')
+print('2 - laranja')
+print('3 - banana')
+produto = int(input('Qual sua escolha?'))
+qtd = int(input('Quantas quantidades?'))
+
+if (produto == 1):
+    pagar = qtd * 2.3
+    print('Você comprol {} maças. Total a pagar R${}' .format(qtd, pagar))
+else:
+    if (produto == 2):
+        pagar = qtd * 3.6
+        print('Você comprol {} laranjas. Total a pagar R${}'.format(qtd, pagar))
+    else:
+        if (produto == 3):
+            pagar = qtd * 1.85
+            print('Você comprol {} banana. Total a pagar R${}'.format(qtd, pagar))
+        else:
+            print('produto unvalido')
