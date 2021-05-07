41dfc9f2fd79c9e1e2f9d5d8112ac233b898f660
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-05-06 22:22:23 -0300

    Create ex1_tupla.py

---
+++ desafio aula6/ex1_tupla.py
@@ -0,0 +1,27 @@
+mochila = ('machado', 'camisa', 'comida', 'aqua')
+print(mochila)
+print(mochila[1])
+print(mochila[0:2]) #elementos 1 e 2, indice 0 e 1
+print(mochila[2:])  #elementos apartir do 2
+print(mochila[-1])  #ultimo elemento
+
+# varendo tupla
+for item in mochila:
+    print('Na mochila tem: {}'.format(item))
+
+#somando
+tupla1 = ('maça', 'banana')
+tupla2 = ('laranja', 'melancia')
+
+#desenpacotamento
+def soma(*num):
+    soma = 0
+    print('tupla: {}'.format(num))
+    for i in num:
+        soma += i
+    return soma
+print('resulatado: {}\n'.format(soma(1,2)))
+print('resulatado: {}\n'.format(soma(1,2,3,4,5,6,7,8,)))
+
+soma = tupla1 + tupla2
+print(soma)
