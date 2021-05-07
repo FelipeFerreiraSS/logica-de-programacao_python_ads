a41246d52defc23d4fd66c488a9d2857bb2a8ff7
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-05-06 23:37:34 -0300

    Create ex4_dicionario.py

---
+++ desafio aula6/ex4_dicionario.py
@@ -0,0 +1,22 @@
+game = {
+    'nome':'Super Mario',
+    'desenvolvedora':'Nintendo',
+    'ano':'1990'
+}
+
+print(game)
+print(game['nome'])
+
+#lista com dicionario
+game = []
+game1 = {
+    'nome':'MArio',
+    'ano':'1990'
+}
+game2 = {
+    'nome':'Zelda',
+    'ano':'1998'
+}
+
+game = [game1, game2]
+print(game)
